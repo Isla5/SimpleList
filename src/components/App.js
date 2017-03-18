b4c963b8a99dac270122ac0826193aa1b78f44f5
Gohar <aria.g@mail.ru>
2017-03-19 01:41:18 +0400

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
 import Countries from './countries.js';
 
-class Search extends React.Component{
-  constructor() {
-          super();
-          this.state = {search: ''};
+class Search extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        };
     }
     render() {
         let Countries = this.props.Countries;
         const search = this.state.search.toLowerCase();
-            Countries = Countries.filter(country => country.name.toLowerCase().match( search ));
-            const rendering =   Countries.map((country,i)=>{
-                return <li key={i}>{country.name} {/*   * search.length = {search.length}   *  country.name.length =  {country.name.length} */}</li>
-            })
-        return <div className = 'searchform'>
-                    <input className='inpfield' type="text" value={this.state.search} onChange={({target}) => this.setState({search: target.value})} placeholder="  Filter..." />
-                    <ul className = 'listc'>
-                        {rendering}
-                    </ul>
-                </div>;
+        Countries = Countries.filter(country => country.name.toLowerCase().match(search));
+        const rendering = Countries.map((country, i) => {
+            return <li key={i}>{country.name}</li>
+        })
+        return <div>
+            <input className='inpfield' type="text" value={this.state.search} onChange={({target}) => this.setState({search: target.value})} placeholder="Filter..."/>
+            <ul className='listc'>
+                {rendering}
+            </ul>
+        </div>;
     }
 };
 
-const App = () => (
-        <Search Countries={ Countries }/>
-);
+const App = () => (<Search Countries={Countries}/>);
 
 export default App;
 
